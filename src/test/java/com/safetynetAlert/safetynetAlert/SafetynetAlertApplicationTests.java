1ee145b00d1cd6de8b15991164e4914e8ab50d0c
SaraSara8 <sara.haimeur84@gmail.com>
2024-07-19 17:03:04 +0200

    initialisation du repository SafeNetAlert

---
+++ src/test/java/com/safetynetAlert/safetynetAlert/SafetynetAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safetynetAlert.safetynetAlert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetynetAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
