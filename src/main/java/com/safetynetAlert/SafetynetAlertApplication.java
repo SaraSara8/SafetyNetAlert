2d84640fd04a3e78ffbc984225aee30e7e440a94
SaraSara8 <sara.haimeur84@gmail.com>
2024-08-05 14:46:38 +0200

    Mise en place du chargement des fichiers

--- src/main/java/com/safetynetAlert/SafetynetAlertApplication.java
+++ src/main/java/com/safetynetAlert/SafetynetAlertApplication.java
@@ -2,53 +2,81 @@ package com.safetynetAlert;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import com.jsoniter.JsonIterator;
+import com.jsoniter.any.Any;
 import com.safetynetAlert.model.JsonDataBase;
+import com.safetynetAlert.model.Person;
+
+
 
 @SpringBootApplication
 public class SafetynetAlertApplication {
 
+	final static Logger logger = LogManager.getLogger(SafetynetAlertApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SafetynetAlertApplication.class, args);
-		
-		
-		JsonDataBase parseTest = new JsonDataBase();
-		
-		
-		
-		
-		String input ="";
-		
-		try {
+
+
+	}
+
+	// mise en place du chargement des données du fichier json dans  l'objet JsonDataBase 
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {  
+			// etape 1 chargement du fichier
+			Path path = Paths.get("src/main/resources/DataBase.json");
+			byte[] fileBites = Files.readAllBytes(path);
 			
-			BufferedReader bufferedreader= 
-					new BufferedReader (new FileReader("/Users/sarahaimeur/Desktop/formations/GIT/Projet_5/safetynetAlert/src/main/resources/DataBase.json"));
-			
-			String line;
-			
-			while ((line = bufferedreader.readLine()) != null) {
-				
-				input += line;
-				
-				
-			}	
-			
-			bufferedreader.close();
-			
-			
-			
-			
-				
-			parseTest.perseDataBase(input);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		
+			// etape 2 etiration avec JsonIter
+			JsonIterator iterator = JsonIterator.parse(fileBites);
+			Any any = iterator.readAny();
+
+			// chargement des persons du  fichier dans l'objet JsonDataBase
+			Any personToRead = any.get("persons");
+			List<Person> personsFromDataBase  = initializePersons(personToRead);
+			logger.info("nombre de personnes chargées depuis le fichier", personsFromDataBase.size());
+
+		};
+
+	}
+
+	private List<Person> initializePersons (Any anyPerson){
+
+		// itereration et remplissage des persons
+
+		List<Person> persons = new ArrayList();
+		anyPerson.forEach(p ->{
+
+			Person person = new Person();
+
+			person.setFirstName(p.get("firstName").toString());
+			person.setLastName(p.get("lastName").toString());
+			person.setCity(p.get("city").toString());
+			person.setPhone(p.get("phone").toString());
+			person.setZip(p.get("zip").toString());
+			person.setEmail(p.get("email").toString());
+
+			persons.add(person);
+
+		});
+
+		return persons;
+
 	}
 
 }
